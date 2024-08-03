ae986859a095802bf808d077717aaa5f8f320e36
romulofull <romulo.castro@gmail.com>
2024-08-02 21:50:42 -0500

    Metdodo: ToUpperCAse,ToLowerCase

--- js/metodosstring.js
+++ js/metodosstring.js
@@ -15,3 +15,12 @@ console.log("frase original es : " + frutas);
 
 let nuevofragmento = frase.substring(7,21);
 console.log (nuevofragmento);
+
+let palabras = "Buenos días a Todos";
+let mayusculas = palabras.toUpperCase();
+let minusculas = palabras.toLowerCase();
+
+console.log("Esta es la frase en Mayuscula : " + mayusculas);
+console.log("Esta es la frase en Minuscula : " + minusculas);
+
+
